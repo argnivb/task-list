706d7e7ce9c68836ff95ea70a3346a7da22058f6
argnivb <abelrecouso@gmail.com>
2022-12-08 14:48:35 -0500

    cleaup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Header } from 'components/Header';
-import { Button } from 'components/Button';
 import { ReactComponent as Logo } from 'assets/favicon.svg';
 
 function App() {
   return (
     <div className="App">
-      <Header title="hola" />
       <Logo height={100} width={100} />
-      <Button onClick={() => alert('hola')}>Hello</Button>
+
+      <h1>Hola Mundo</h1>
     </div>
   );
 }
