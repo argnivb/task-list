2dff2e0238843cd7ee2d1c98907cebefdafca145
argnivb <abelrecouso@gmail.com>
2022-12-09 21:45:46 -0500

    ButtonBar responsive behavior

--- src/components/ButtonBar/ButtonBar.tsx
+++ src/components/ButtonBar/ButtonBar.tsx
@@ -4,33 +4,43 @@ import { ReactComponent as Calendar } from '../../assets/calendar.svg';
 import { ReactComponent as Unlock } from '../../assets/unlock.svg';
 import { ReactComponent as Sun } from '../../assets/sun.svg';
 import { ReactComponent as Circle } from '../../assets/circle.svg';
+import { ReactComponent as Close } from '../../assets/x.svg';
 
 export const ButtonBar = () => {
   return (
     <div className="p-2 border flex justify-between rounded-b bg-[#FAFBFB]">
       <div className="flex">
         <Button type="secundary">
-          <Maximize className="w-[20px] h-[20px] mr-3 ml-[-8px]" /> open
+          <Maximize className="w-[20px] h-[20px] lg:mr-3 lg:ml-[-8px]" />
+          <span className="hidden lg:inline">open</span>
         </Button>
         <Button type="outline" className="ml-8">
-          <Calendar className="w-[20px] h-[22px] mr-3 ml-[-8px]" /> today
+          <Calendar className="w-[20px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
+          <span className="hidden lg:inline">today</span>
         </Button>
         <Button type="outline" className="ml-1">
-          <Unlock className="w-[20px] h-[22px] mr-3 ml-[-8px]" /> public
+          <Unlock className="w-[20px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
+          <span className="hidden lg:inline">public</span>
         </Button>
         <Button type="outline" className="ml-1">
-          <Sun className="w-[22px] h-[22px] mr-3 ml-[-8px]" /> highlight
+          <Sun className="w-[22px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
+          <span className="hidden lg:inline">highlight</span>
         </Button>
         <Button type="outline" className="ml-1">
-          <span className="flex items-center justify-center relative mr-5 ml-[-4px]">
+          <span className="flex items-center justify-center relative lg:mr-5 lg:ml-[-4px]">
             <Circle className="absolute" /> 0
           </span>
-          estimation
+          <span className="hidden lg:inline">estimation</span>
         </Button>
       </div>
       <div className="flex">
-        <Button type="secundary">cancel</Button>
-        <Button className="ml-1">ok</Button>
+        <Button type="secundary" className="max-lg:!hidden">
+          cancel
+        </Button>
+        <Button className="ml-1 max-lg:!hidden">ok</Button>
+        <Button className="ml-1 lg:!hidden">
+          <Close className="w-[24px] h-[24px]" />
+        </Button>
       </div>
     </div>
   );
