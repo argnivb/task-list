43d1ddd022ddd36a84ae6bc1311b70e9ee1b1c34
argnivb <abelrecouso@gmail.com>
2022-12-09 22:21:01 -0500

    tailwind config use Roboto fontFamily instead

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 // eslint-disable-next-line no-undef
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+// eslint-disable-next-line no-undef
 module.exports = {
   content: ['./src/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
@@ -6,6 +9,9 @@ module.exports = {
       colors: {
         primary: '#0D55CF',
         secondry: '#EAF0F5',
+      },
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
       },
     },
   },
