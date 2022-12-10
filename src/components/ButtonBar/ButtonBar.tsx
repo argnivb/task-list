e6067f505fcfeeaa0033f011310f21ba7da2d6cd
argnivb <abelrecouso@gmail.com>
2022-12-10 15:29:30 -0500

    ButtonBar: receive save & cancel handlers and disabled state

--- src/components/ButtonBar/ButtonBar.tsx
+++ src/components/ButtonBar/ButtonBar.tsx
@@ -5,28 +5,43 @@ import { ReactComponent as Unlock } from '../../assets/unlock.svg';
 import { ReactComponent as Sun } from '../../assets/sun.svg';
 import { ReactComponent as Circle } from '../../assets/circle.svg';
 import { ReactComponent as Close } from '../../assets/x.svg';
+import { ReactComponent as Save } from '../../assets/save.svg';
 
-const ButtonBar = () => {
+const ButtonBar = ({
+  onCancel,
+  onSave,
+  disabled = false,
+  isEditing = false,
+}: {
+  onCancel: () => void;
+  onSave: () => void;
+  disabled?: boolean;
+  isEditing?: boolean;
+}) => {
+  const shouldDisplaySaveIcon = isEditing && !disabled;
+  const shouldDisplayCloseIcon = disabled;
+  const shouldDisplayPlusIcon = !isEditing && !disabled;
+
   return (
     <div className="p-2 border flex justify-between rounded-b bg-[#FAFBFB]">
       <div className="flex">
-        <Button type="secundary">
+        <Button type="secundary" disabled={disabled}>
           <Maximize className="w-[20px] h-[20px] lg:mr-3 lg:ml-[-8px]" />
           <span className="hidden lg:inline">open</span>
         </Button>
-        <Button type="outline" className="ml-8">
+        <Button type="outline" className="ml-8" disabled={disabled}>
           <Calendar className="w-[20px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
           <span className="hidden lg:inline">today</span>
         </Button>
-        <Button type="outline" className="ml-1">
+        <Button type="outline" className="ml-1" disabled={disabled}>
           <Unlock className="w-[20px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
           <span className="hidden lg:inline">public</span>
         </Button>
-        <Button type="outline" className="ml-1">
+        <Button type="outline" className="ml-1" disabled={disabled}>
           <Sun className="w-[22px] h-[22px] lg:mr-3 lg:ml-[-8px]" />
           <span className="hidden lg:inline">highlight</span>
         </Button>
-        <Button type="outline" className="ml-1">
+        <Button type="outline" className="ml-1" disabled={disabled}>
           <span className="flex items-center justify-center relative lg:mr-5 lg:ml-[-4px]">
             <Circle className="absolute" /> 0
           </span>
@@ -34,12 +49,18 @@ const ButtonBar = () => {
         </Button>
       </div>
       <div className="flex">
-        <Button type="secundary" className="max-lg:!hidden">
+        <Button type="secundary" className="max-lg:!hidden" onClick={onCancel}>
           cancel
         </Button>
-        <Button className="ml-1 max-lg:!hidden">ok</Button>
+        <Button className="ml-1 max-lg:!hidden" onClick={onSave}>
+          ok
+        </Button>
         <Button className="ml-1 lg:!hidden">
-          <Close className="w-[24px] h-[24px]" />
+          {shouldDisplayCloseIcon && <Close className="w-[24px] h-[24px]" onClick={onCancel} />}
+          {shouldDisplaySaveIcon && <Save className="w-[24px] h-[24px]" onClick={onSave} />}
+          {shouldDisplayPlusIcon && (
+            <Close className="w-[24px] h-[24px] rotate-45" onClick={onSave} />
+          )}
         </Button>
       </div>
     </div>
